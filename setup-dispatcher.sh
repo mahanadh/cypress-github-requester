599ad6e2cb2dd6014906f69bddd723300f2fc121
Mahan Adhikari <mahan2054@gmail.com>
2023-10-16 15:51:28 +0545

    Add cypress, github actions and workflow requester

---
+++ setup-dispatcher.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Trigger remote repository using github API with user defined event_type: "new-code-has-been-added"
+# Client PayLoad can include a host of parameters, variables to be exported to the sputnik-test-automation
+curl -H "Accept: application/vnd.github.everest-preview+json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    --request POST \
+    --data '{"event_type": "Test-Dispatch", "client_payload": { "text": "A new code has been pushed into this repository"}}' \
+    https://api.github.com/repos/cloudfactory/sputnik-test-automation/dispatches
