2117bfcf1a55b66720d9ffbd6ce31fd99c743772
Mahan Adhikari <mahan2054@gmail.com>
2023-10-17 13:47:48 +0545

    Add PR number dynamically

--- setup-dispatcher.sh
+++ setup-dispatcher.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Read the PR_NUMBER environment variable
+PR_NUMBER=$PR_NUMBER
+
 # Trigger remote repository using github API with user defined event_type: "new-code-has-been-added"
 # Client PayLoad can include a host of parameters, variables to be exported to the sputnik-test-automation
 curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token $GITHUB_TOKEN" \
     --request POST \
-    --data '{"event_type": "Test-Dispatch", "client_payload": { "text": "A new code has been pushed into this repository"}}' \
+    --data '{"event_type": "Test-Dispatch", "client_payload": { 
+        "text": "Label has been attached",
+        "repository_name": "cypress-github-requestor",
+        "pr_number": "'"$PR_NUMBER"'"
+        }
+        }' \
     https://api.github.com/repos/mahanadh/cypress-github-acceptor/dispatches
